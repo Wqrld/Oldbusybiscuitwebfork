eba0d31ff46ffa5411a25edaac6cebadb2034799
mrCookieSlime <mrCookieSlime@gmail.com>
2015-04-10 13:11:43 +0200

    Fixed Search

--- javascript/search.js
+++ javascript/search.js
@@ -1,2 +1,32 @@
-<script type="text/javascript" src="https://dl.dropboxusercontent.com/s/gv9ol503vdrjcgf/search.js"></script>
-Search: <input size="30" type="text" id="search" class="search_box" onkeyup="search()" value="Browse Projects" onfocus="if(this.value == 'Browse Projects'){this.value='';}" />
+function search() {
+    var search = document.getElementById('search').value;
+    var table = document.getElementById('projects');
+    var total;
+     
+    for (var i = 0; i < table.rows.length; i++) {
+        var row = '';
+         
+        if (i == 0) {
+            total = table.rows.item(i).cells.length;
+            continue;
+        }
+         
+        for (var j = 0; j < total; j++) {
+            var text = '';
+             
+            if (navigator.appName == 'Microsoft Internet Explorer')
+                text = table.rows.item(i).cells.item(j).innerText;
+            else
+                text = table.rows.item(i).cells.item(j).textContent;
+                row += text;
+        }
+         
+        row = row.toLowerCase();
+        search = search.toLowerCase();
+         
+        if (row.indexOf(search) == -1)
+            table.rows.item(i).style.display = 'none';
+        else
+            table.rows.item(i).style.display = 'table-row';
+    }
+}
